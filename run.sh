50fd7b7a7fb45641296bab045163eb1b1508d200
Thomas Miller <thomasmiller107@gmail.com>
2022-11-03 17:48:13 -0400

    Adding interactive mode option

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm data/debug_*.csv
 export $(cat .env | xargs)
-/usr/local/bin/python3 automation.py
+#/usr/local/bin/python3 -i automation.py # Run in interactive mode
+/usr/local/bin/python3 automation.py # Run in regular mode
