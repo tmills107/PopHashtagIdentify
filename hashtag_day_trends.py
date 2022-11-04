792e01505718db91a2a673ff438c8c765158c820
Thomas Miller <thomasmiller107@gmail.com>
2022-11-04 12:45:11 -0400

    fixing outputs

--- hashtag_day_trends.py
+++ hashtag_day_trends.py
@@ -1,40 +1,45 @@
 import tweepy
 import os
 import pandas as pd
+from datetime import datetime
+from datetime import timedelta
 
 bearer_token = os.environ.get('BEARER_TOKEN')
 
 client = tweepy.Client(bearer_token = bearer_token,
     return_type=dict)
 
+#################################################################
 hashtag_list = ["takeoff", "blacktwitter", "kanyewest", "blacklivesmatter", "halloween", "gay", "lgbtq", "lgbt", "pride", "voteblue", "vote", "bluewave", "bluecrew", "votebluetoprotectyourrights", "trump", "maga", "elonmusk", "gop", "biden"]
+time_year, time_month, time_day = (2022, 11, 1)
+#################################################################
 
-hashtag_count=[]
+def query_hashtag_counts(_hashtag, _start_time, _end_time):
+        hashtag_count_hourly = []
+        count = client.get_recent_tweets_count(query= "#" + _hashtag,
+            granularity = 'hour',
+            start_time=_start_time,
+            end_time=_end_time)
+        
+        for item in count['data']:
+            __start_time = datetime.strftime(datetime.strptime(item['start'], '%Y-%m-%dT%H:%M:%S.%f%z'), '%Y_%m_%d_%H')
+            __end_time = datetime.strftime(datetime.strptime(item['end'], '%Y-%m-%dT%H:%M:%S.%f%z'), '%Y_%m_%d_%H')
+            __count = item['tweet_count']
+            hashtag_count_hourly.append([hashtag, __start_time, __end_time, __count])
+        df = pd.DataFrame(hashtag_count_hourly, columns = ["hashtag", "start time", "end time", "count"])
+        return df
 
-#STARTTIME = '2022-10-25T08:00:00-04:00'
-ENDTIME = '2022-11-01T22:00:00-04:00'
+
+datetime_day = datetime(time_year, time_month, time_day) # First moment of day to check
+ENDTIME = datetime_day + timedelta(hours=22)
+STARTTIME = datetime_day + timedelta(hours=8)
+
+all_df =[]
 
 for hashtag in hashtag_list:
-    count = client.get_recent_tweets_count(query= "#" + hashtag + ' -is:retweet -is:quote lang:en',
-        granularity = 'day',
-        #start_time = STARTTIME,
-        end_time = ENDTIME)
-    
-    tweet_count_by_hour = []
-    day = 0
-    item_time_count = 00
-    for item in count['data']:
-        tweet_count = count['data'][item_time_count]['tweet_count']
-        tweet_count_by_hour.append((day,tweet_count))
-        day = day+1 
-        item_time_count= item_time_count + 1
-    
-    total_tweet_count = count['meta']['total_tweet_count']
+    df = query_hashtag_counts(hashtag, STARTTIME, ENDTIME)
+    all_df.append(df)
 
-    hashtag_count.append((hashtag, tweet_count_by_hour, total_tweet_count))
+all_df = pd.concat(all_df)
 
-sorted_hashtag_count = sorted(hashtag_count, key = lambda kv:kv[2], reverse=True)
-
-df_all_twitter = pd.DataFrame(sorted_hashtag_count, columns=["hashtag", "Daily" , "Total"])
-df_all_twitter.sort_values("Total", inplace=True, ascending=False)
-df_all_twitter.to_csv('./data/all_twitter_hashtag_count_top.csv', index=False)
+all_df.to_csv('hashtag_daily_count.csv', index=False)
