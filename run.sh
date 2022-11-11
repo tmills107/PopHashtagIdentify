453d159ab1882061aa869cc1b78856c389b0c102
Thomas Miller <thomasmiller107@gmail.com>
2022-11-11 16:13:46 -0500

    Updating env, script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 conda activate twitter_hashtag_analysis
 rm data/debug_*.csv
+rm data/debug_*.png
 export $(cat .env | xargs)
 #python -i automation.py # Run in interactive mode
 python automation.py # Run in regular mode
